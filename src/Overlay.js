2eeed2f182faa2eb1931539d5d32beab9846a350
gottaegbert <gottaegbert@gmail.com>
2021-05-31 13:51:59 +0800

    build

--- src/Overlay.js
+++ src/Overlay.js
@@ -1,5 +1,5 @@
 import React from 'react'
-// import { Footer } from '@pmndrs/branding'
+import { Footer } from '@pmndrs/branding'
 
 export default function Overlay({ ready, clicked, setClicked }) {
   return (
@@ -7,12 +7,13 @@ export default function Overlay({ ready, clicked, setClicked }) {
       <div className={`fullscreen bg ${ready ? 'ready' : 'notready'} ${clicked && 'clicked'}`}>
         <div onClick={() => ready && setClicked(true)}>{!ready ? 'loading' : 'click to continue'}</div>
       </div>
-      {/* <Footer
+      {/* <div link1={<a href="https://github.com/pmndrs/drei">@gottaegbert</a>} /> */}
+      <Footer
         date="6.3"
         year="2021"
-        link1={<a href="https://github.com/pmndrs/drei">@gottaegbert</a>}
-        link2={<a href="https://codesandbox.io/s/drei-reflector-bfplr">weibotrending</a>}
-      /> */}
+        link1={<a href="#">@gottaegbert</a>}
+        link2={<a href="#">weibotrending</a>}
+      />
     </>
   )
 }

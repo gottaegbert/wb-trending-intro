41ae66a7a4066d38f2f27a3dde6bee71c5ab6ab1
Egbert HU <62612263+gottaegbert@users.noreply.github.com>
2023-08-17 19:17:16 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import * as THREE from 'three'
-import React, { Suspense, useEffect, useState,useMemo,useRef } from 'react'
+import React, { Suspense, useEffect, useState,useRef } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
 import { Reflector, Text, useTexture, useGLTF } from '@react-three/drei'
-import { EffectComposer,ChromaticAberration, DepthOfField, Bloom, Noise, Vignette } from '@react-three/postprocessing'
 // import { useSpring, animated,config } from '@react-spring/three'
 // import Overlay from './Overlay'
 // import randomWord from 'random-words'
@@ -20,51 +19,6 @@ function Carla3(props) {
   return <primitive object={scene}  {...props} />
 }
 
-function Overlay() { 
-  return (
-    <>
-      
-      <a href="https://gottaegbert.github.io/weibo-trend/">Explore it now</a>
-    </>
-)
-
-}
-// function Item({ children, ...props }) {
-//   const ref = useRef()
-//   const [hovered, setHovered] = useState(false)
-//   useFrame(({ camera }) => ref.current.quaternion.copy(camera.quaternion))
-//   return (
-//     <Text
-//       onPointerOver={(e) => (e.stopPropagation(), setHovered(true))}
-//       onPointerOut={() => setHovered(false)}
-//       ref={ref}
-//       {...props}
-//       color={hovered ? 'hotpink' : 'white'}
-//       material-toneMapped={false}
-//       font="/Inter-Bold.woff"
-//       fontSize={2.5}
-//       letterSpacing={-0.05}>
-//       {children}
-//     </Text>
-//   )
-// }
-// function Cloud({ count = 100, distance = 20 }) {
-//   const data = useMemo(() => {
-//     const temp = []
-//     for (var i = 0; i < count; i++) {
-//       let vertex = new THREE.Vector3()
-//       let theta = THREE.MathUtils.randFloatSpread(360)
-//       let phi = THREE.MathUtils.randFloatSpread(360)
-//       temp.push({
-//         position: vertex.set(distance * Math.sin(theta) * Math.cos(phi), distance * Math.sin(theta) * Math.sin(phi), distance * Math.cos(theta)),
-//         children: randomWord()
-//       })
-//     }
-//     return temp
-//   }, [count, distance])
-//   return data.map((props, index) => <Item {...props} key={index} />)
-// }
-
 
 function VideoText({ clicked, ...props }) {
   const [active, setActive] = useState(false);
@@ -81,11 +35,6 @@ function VideoText({ clicked, ...props }) {
   });
 
   return (
-    // <Text font="/Inter-Bold.woff" fontSize={1} letterSpacing={-0.1} outlineBlur={0.2} {...props}>
-    //   Trending         //TODO: 做账贴图放到视频末尾
-    //   <meshBasicMaterial toneMapped={false}>
-    //     <videoTexture  attach="map" args={[video]} encoding={THREE.sRGBEncoding} />
-    //   </meshBasicMaterial>
       <Text ref={myMesh} font="/webfont.woff" fontSize={2} letterSpacing={0}outlineBlur={0.2} {...props}>
       数字情绪
        <Text ref={myMesh} font="/webfont.woff" fontSize={2} letterSpacing={-0.1}outlineBlur={0.2} {...props}></Text>
@@ -116,36 +65,17 @@ function Intro({ start, set }) {
     }
   })
 }
-// function Box(props) {
-//   // This reference will give us direct access to the mesh
-//   const mesh = useRef()
-//   // Set up state for the hovered and active state
-//   const [hovered, setHover] = useState(false)
-//   const [active, setActive] = useState(false)
-//   // Subscribe this component to the render-loop, rotate the mesh every frame
-//   useFrame((state, delta) => (mesh.current.rotation.x += 0.01))
-//   // Return view, these are regular three.js elements expressed in JSX
-//   return (
-//     <mesh
-//       {...props}
-//       ref={mesh}
-//       scale={active ? 0.5 : 0.3}
-//       onClick={(event) => setActive(!active)}
-//       onPointerOver={(event) => setHover(true)}
-//       onPointerOut={(event) => setHover(false)}
-//     >
-//       <boxGeometry args={[1, 1, 1]} />
-//       <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
-//     </mesh>
-//   )
-// }
+
 export default function App() {
   const [clicked, setClicked] = useState(false)
   const [ready, setReady] = useState(false)
   const store = { clicked, setClicked, ready, setReady }
+  const ref = useRef()
   return (
-    <>
-      <Overlay />
+    <div ref={ref} className="container">
+      <div className="text">
+        Expore it now!
+        </div>
       <Canvas concurrent gl={{ alpha: false }} pixelRatio={[1, 1.5]} camera={{ position: [0, 3, 240], fov: 35 }}>
         <color attach="background" args={['black']} />
         <fog attach="fog" args={['black', 15, 20]} />
@@ -171,7 +101,6 @@ export default function App() {
         </Suspense>
 
       </Canvas>
-      <Overlay />
-    </>
+</div>
   )
 }
